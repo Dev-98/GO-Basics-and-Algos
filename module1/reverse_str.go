538cb7e104673131e9bdb87f052e04b784ded76f
Dev-98 <devparker48@gmail.com>
2024-08-16 17:26:35 +0530

    Sum of values in list function and its testing file to check the function

---
+++ module1/reverse_str.go
@@ -0,0 +1,20 @@
+package module1
+
+// import "fmt"
+
+func ReverseString(s string) (ans string) {
+	last := len(s) - 1
+	for i := range s {
+		// fmt.Printf("%v", i)
+		ans += string(s[last-i])
+	}
+
+	return ans
+}
+
+func Reverse(s string) (ans string) {
+	for _, r := range s {
+		ans = string(r) + ans
+	}
+	return ans
+}
---
+++ module1/reverse_str_test.go
@@ -0,0 +1,32 @@
+package module1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		word string
+		want string
+	}{
+		{"cat", "tac"},
+		{"malla", "allam"},
+		{"dog", "god"},
+		{"dev", "ved"},
+
+		{"alphabet", "tebahpla"},
+		{"日本語", "語本日"},
+	}
+
+	for _, tc := range tests {
+		t.Run(fmt.Sprintf("%v", tc.word), func(t *testing.T) {
+			ans := Reverse(tc.word)
+			if ans != tc.want {
+				t.Fatalf("NumInList() = %v; want %v", ans, tc.want)
+			}
+		})
+
+	}
+
+}
